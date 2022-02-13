630e5b3d7fe6ff510d4eb66b926a914e9c4b06c0
Jiří Vrba <jiri@vrba.dev>
2022-02-13 08:28:40 +0100

    Resharper goes brrrrrr

--- src/PaulBot/Program.cs
+++ src/PaulBot/Program.cs
@@ -1,6 +1,6 @@
 using PaulBot;
 
-IHost host = Host.CreateDefaultBuilder(args)
+var host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services => { services.AddHostedService<Worker>(); })
     .Build();
 
