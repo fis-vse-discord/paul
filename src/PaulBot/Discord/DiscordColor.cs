0f2aef86b0384a57cdbd64931c2c9fe1ed4c2e37
Jiří Vrba <jiri@vrba.dev>
2022-02-13 15:47:36 +0100

    Add discord color constants

---
+++ src/PaulBot/Discord/DiscordColor.cs
@@ -0,0 +1,14 @@
+namespace PaulBot.Discord;
+
+public class DiscordColor
+{
+    public const int Primary = 0x5865f2;
+
+    public const int Green = 0x57f287;
+
+    public const int Yellow = 0xfee75c;
+
+    public const int Fuchsia = 0xeb459e;
+
+    public const int Red = 0xed4245;
+}
