26877e70d2ab4fcd7785d0ed44d7b2871aeb9ac7
Jiří Vrba <jiri@vrba.dev>
2022-02-13 13:34:22 +0100

    Add interactions framework on application startup

--- src/PaulBot/Configuration/DiscordConfiguration.cs
+++ src/PaulBot/Configuration/DiscordConfiguration.cs
@@ -4,5 +4,13 @@ public class DiscordConfiguration
 {
     public const string Section = "Discord";
     
+    /// <summary>
+    /// Token used for the Discord gateway authentication
+    /// </summary>
     public string Token { get; init; } = null!;
+
+    /// <summary>
+    /// ID of the guild to which the slash commands should be registered
+    /// </summary>
+    public ulong GuildId { get; init; }
 }
--- src/PaulBot/Discord/DiscordBotService.cs
+++ src/PaulBot/Discord/DiscordBotService.cs
@@ -1,4 +1,6 @@
+using System.Reflection;
 using Discord;
+using Discord.Interactions;
 using Discord.WebSocket;
 using Microsoft.Extensions.Options;
 using PaulBot.Configuration;
@@ -8,30 +10,59 @@ namespace PaulBot.Discord;
 public class DiscordBotService : BackgroundService
 {
     private readonly ILogger<DiscordBotService> _logger;
-    
+
     private readonly DiscordSocketClient _client;
-    
+
     private readonly DiscordConfiguration _configuration;
 
-    public DiscordBotService(ILogger<DiscordBotService> logger, DiscordSocketClient client, IOptions<DiscordConfiguration> configuration)
+    private readonly IServiceProvider _services;
+
+    private readonly InteractionService _interactions;
+
+    public DiscordBotService(
+        ILogger<DiscordBotService> logger,
+        DiscordSocketClient client,
+        IOptions<DiscordConfiguration> configuration,
+        IServiceProvider services,
+        IHostEnvironment environment
+    )
     {
         _logger = logger;
         _client = client;
+        _services = services;
         _configuration = configuration.Value;
+        _interactions = new InteractionService(_client);
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         _client.Log += LogMessageAsync;
-        
+        _client.Ready += RegisterInteractionsAsync;
+        _client.InteractionCreated += HandleSocketInteractionAsync;
+
         await _client.LoginAsync(TokenType.Bot, _configuration.Token);
         await _client.StartAsync();
+        
         await Task.Delay(-1, stoppingToken);
+    }
+
+    private async Task RegisterInteractionsAsync()
+    {
+        await _interactions.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+        await _interactions.RegisterCommandsToGuildAsync(_configuration.GuildId);
     }
 
     private Task LogMessageAsync(LogMessage message)
     {
         _logger.LogInformation("[Discord]: {message}", message.ToString());
         return Task.CompletedTask;
+    }
+
+    private async Task HandleSocketInteractionAsync(SocketInteraction interaction)
+    {
+        var context = new SocketInteractionContext(_client, interaction);
+        var result = await _interactions.ExecuteCommandAsync(context, _services);
+        
+        // TODO: Handle result  
     }
 }
