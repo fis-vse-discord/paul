2ab349a0afd58b75a03c4067179bf864cadd04ca
Jiří Vrba <jiri@vrba.dev>
2022-02-16 20:01:13 +0100

    Add work in progress Subject model definition

---
+++ src/PaulBot/Discord/Subjects/Models/Subject.cs
@@ -0,0 +1,32 @@
+namespace PaulBot.Discord.Subjects.Models;
+
+public class Subject
+{
+    public Subject(string code, string name)
+    {
+        Code = code;
+        Name = name;
+    }
+    
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Code of the subject (eg. 4IZ101)
+    /// </summary>
+    public string Code { get; init; }
+
+    /// <summary>
+    /// Name of the subject (eg. "Programming")
+    /// </summary>
+    public string Name { get; init; }
+    
+    /// <summary>
+    /// ID of the mapped Discord channel
+    /// </summary>
+    public ulong ChannelId { get; set; }
+    
+    /// <summary>
+    /// ID of the mapped Discord role
+    /// </summary>
+    public ulong RoleId { get; set; }
+}
