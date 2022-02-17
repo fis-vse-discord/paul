d8147f4a475d4b7c9417e0ea0798162261c160fe
Jiří Vrba <jiri@vrba.dev>
2022-02-17 11:44:58 +0100

    /delete-subject slash command interface definition

--- src/PaulBot/Discord/Subjects/SubjectsInteractionsModule.cs
+++ src/PaulBot/Discord/Subjects/SubjectsInteractionsModule.cs
@@ -37,4 +37,25 @@ public class SubjectsInteractionsModule : InteractionModuleBase<SocketInteractio
 
         await FollowupAsync(embed: embed);
     }
+
+    [RequireUserPermission(GuildPermission.ManageGuild)]
+    [RequireBotPermission(GuildPermission.ManageChannels | GuildPermission.ManageRoles)]
+    [SlashCommand("delete-subject", "Deleted the specified subject and the associated role (and optionally channel)")]
+    public async Task DeleteSubjectAsync(
+        [Summary("code", "Code of the subject that should be deleted")] string code,
+        [Summary("delete-text-channel", "Set this to true to also delete the associated text channel")] 
+        bool deleteTextChannel = false
+    )
+    {
+        await DeferAsync();
+        
+        var subject = await _service.DeleteSubject(code, deleteTextChannel);
+        var embed = new EmbedBuilder()
+            .WithColor(DiscordColor.Green)
+            .WithTitle($"Subject {subject.Code} deleted")
+            .WithCurrentTimestamp()
+            .Build();
+
+        await FollowupAsync(embed: embed);
+    }
 }
